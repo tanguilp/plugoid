ebc629b455cd8bad1d2ddc2f2dbf31d2f39af646
Tangui <29804907+tanguilp@users.noreply.github.com>
2021-09-22 21:26:05 +0300

    Fix a bug in ID token hashing verification (OIDC lib)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 
 The format is based on Keep a Changelog and this project adheres to Semantic Versioning.
 
+## [0.4.2] - 2021-09-22
+
+### Fixed
+
+- [`Plugoid`] Fixed a bug with token hash validation in imported library
+
 ## [0.4.1] - 2020-10-16
 
 ### Fixed
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Plugoid.MixProject do
     [
       app: :plugoid,
       description: "OpenID Connect Plug for Phoenix",
-      version: "0.4.1",
+      version: "0.4.2",
       elixir: "~> 1.9",
       compilers: [:phoenix] ++ Mix.compilers,
       start_permanent: Mix.env() == :prod,
@@ -35,7 +35,7 @@ defmodule Plugoid.MixProject do
       {:jwks_uri_updater, "~> 1.1"},
       {:oauth2_metadata_updater, "~> 1.2"},
       {:oauth2_utils, "~> 0.1"},
-      {:oidc, "~> 0.3"},
+      {:oidc, "~> 0.4"},
       {:phoenix_html, "~> 2.0"},
       {:phoenix, "~> 1.0"}
     ]
